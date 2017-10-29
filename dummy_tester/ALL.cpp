09900f25edd11c6077f570f541d6fff873d234ca
Arasid <basa.akrobat@gmail.com>
2017-10-29 09:39:42 +0100

    Remove dummy tester.

--- dummy_tester/ALL.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-using namespace std;
-
-int main(){
-  // Because of it's name, this file causes dummy tester to yield a protocol with all kinds of responses
-
-  return 0;
-}
--- dummy_tester/OK.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-using namespace std;
-
-int main(){
-  // Because of it's name, this file causes dummy tester to yield a OK protocol
-
-  return 0;
-}
