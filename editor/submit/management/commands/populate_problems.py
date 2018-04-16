f8d2dd6bd0bb78386dd009ac187807fb81e5cf94
Arasid <basa.akrobat@gmail.com>
2018-04-16 12:13:34 +0200

    Pridaj html zadanie pre 1,2. Populuj z html.

--- editor/submit/management/commands/populate_problems.py
+++ editor/submit/management/commands/populate_problems.py
@@ -23,28 +23,44 @@ class Command(BaseCommand):
             problem_id = int(os.path.dirname(input_path).split('/')[-1])
             if problem_id < 1: continue
 
+            dir = os.path.dirname(input_path)
+            print(os.path.join(dir, 'nazov.txt'))
+
+            json_txt = '{}'
+            nazov = 'Uloha %d' % problem_id
+            content = 'Zadanie chýba'
             with open(input_path, 'r') as json_file:
                 json_txt = json_file.read()
+            try:
+                with open(os.path.join(dir, 'nazov.txt'), 'r') as inp:
+                    nazov = inp.readline().strip()
+            except:
+                pass
+            try:
+                with open(os.path.join(dir, 'zadanie.html'), 'r') as inp:
+                    content = inp.read()
+            except:
+                pass
 
-                problem = Problem.objects.filter(id=problem_id).first()
+            problem = Problem.objects.filter(id=problem_id).first()
 
-                if problem is None:
-                    while order in used_orders: order += 1
-                    problem, _ = Problem.objects.update_or_create(
-                        id=problem_id,
-                        title='Uloha %d' % problem_id,
-                        content='a',
-                        variables=json_txt,
-                        order=order,
-                    )
-                    self.stdout.write(self.style.NOTICE('Created %s problem' % problem.title))
-                    order += 1
-                else:
-                    Problem.objects.update_or_create(
-                        id=problem_id,
-                        variables=json_txt,
-                    )
-                    self.stdout.write(self.style.NOTICE('Updated %s problem' % problem.title))
+            if problem is None:
+                while order in used_orders: order += 1
+                problem, _ = Problem.objects.update_or_create(
+                    id=problem_id,
+                    title=nazov,
+                    content=content,
+                    variables=json_txt,
+                    order=order,
+                )
+                self.stdout.write(self.style.NOTICE('Created %s problem' % problem.title))
+                order += 1
+            else:
+                problem.variables=json_txt
+                problem.title = nazov
+                problem.content=content
+                problem.save()
+                self.stdout.write(self.style.NOTICE('Updated %s problem' % problem.title))
 
     def handle(self, *args, **options):
         self._populate_problems(options['path_to_test_dir'])
--- hard_reset.sh
+++ hard_reset.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -r judge/tmp/
+mkdir judge/tmp/
 rm -r judge/submits
 rm -r editor/editor/submit/
 rm editor/db.sqlite3
